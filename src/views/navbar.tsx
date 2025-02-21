840080ae0068439c3e9193c6454dcd6f6606e76e
Lucas Badino <lucas.badino.7@gmail.com>
2025-02-21 10:41:00 -0300

    add some styles

--- src/views/navbar.tsx
+++ src/views/navbar.tsx
@@ -2,15 +2,18 @@ export const Navbar = () => {
     return (
         <nav
             className="bg-gray-800 py-4 fixed top-0 w-full flex items-center justify-center"
-            style={{ backgroundColor: "#f5f0dbd8" }}
+            style={{ 
+                backgroundColor: "#edf0c9",
+                opacity: "0.7",
+            }}
         >
             <div className="container mx-auto flex items-center justify-center">
                 <ul className="flex space-x-4 m-auto rounded-2xl p-2 shadow-lg">
                     <li>Home</li>
-                    <li>About Me</li>
+                    <li>About</li>
                     <li>Projects</li>
-                    <li>peter</li>
-                    <li>peter</li>
+                    <li>Articles</li>
+                    <li>Contact</li>
                 </ul>
             </div>
         </nav>
